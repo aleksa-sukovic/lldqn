35fe69161bf0347e83b24fe75c856fc077a7c698
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2022-06-11 23:09:31 +0200

    Adds evaluation capabilities to autoencoder

--- src/models/invariant_representation.py
+++ src/models/invariant_representation.py
@@ -46,29 +46,43 @@ class InvariantRepresentation:
         self, task: "Task", save_data_dir: str = "", save_model_name: str = ""
     ) -> None:
         self.task: "Task" = task
-        self.task_state_space: int = self._get_state_space(task)
         self.save_data_dir = save_data_dir
         self.save_model_name = f"{save_model_name}.pt"
         self.autoencoder: Autoencoder = None
-        self.train_epochs = 150
+        self.state_tuple_dim = (
+            10  # Large enough value to fit the state-space tuple of all tasks.
+        )
+        self.train_epochs = 250
         self.train_batch = 64
+        self.test_batch = 64
         self.learning_rate = 1e-3
         self.weight_decay = 1e-5
+        self.task_state_space: int = self._get_state_space(task)
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    def load(self):
+        self.autoencoder = Autoencoder(self.task_state_space).to(self.device)
+        self.autoencoder.load_state_dict(
+            torch.load(join(self.save_data_dir, self.save_model_name))
+        )
+        self.autoencoder.eval()
 
     def train(self, batches: np.ndarray[Any, Batch]) -> None:
         dataset = self._get_dataset(batches)
         dataloader = DataLoader(dataset, batch_size=self.train_batch, shuffle=True)
-        model = Autoencoder(self.task_state_space).cuda()
+        model = Autoencoder(self.task_state_space).to(self.device)
         criterion = nn.MSELoss()
         optimizer = torch.optim.Adam(
             model.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay
         )
+
         progress = tqdm.tqdm(total=self.train_epochs)
+        progress.write(f"Training autoencoder for '{self.task.name}'")
 
         for _ in range(self.train_epochs):
             for data in dataloader:
                 batch = data[0]
-                batch = torch.autograd.Variable(batch).cuda()
+                batch = batch.to(self.device)
 
                 output = model(batch)
                 loss = criterion(output, batch)
@@ -83,22 +97,38 @@ class InvariantRepresentation:
         torch.save(model.state_dict(), join(self.save_data_dir, self.save_model_name))
 
     def evaluate(self, batches: np.ndarray[Any, Batch]) -> int:
-        # TODO: Ensure batches are zero-padded to match this particular task.
-        pass
+        dataset = self._get_dataset(batches)
+        dataloader = DataLoader(dataset, batch_size=self.test_batch, shuffle=True)
+        criterion = nn.MSELoss()
+        result = 0.0
+
+        for data in dataloader:
+            batch = data[0].to(self.device)
+            output = self.autoencoder(batch)
+            loss = criterion(output, batch)
+            result += loss.item()
+
+        return result / len(dataloader)
 
     def _get_dataset(self, batches: np.ndarray[Any, Batch]) -> Dataset:
         result = np.empty((batches.shape[0], self.task_state_space))
         for (index, batch) in enumerate(batches):
-            experience_tuple = [
+            tuple = [
                 batch.obs,
                 batch.act.reshape(-1),
                 batch.rew.reshape(-1),
                 batch.obs_next,
             ]
-            result[index] = np.concatenate(experience_tuple, axis=0)
-        return TensorDataset(torch.Tensor(result))
+            tuple = np.concatenate(tuple, axis=0)
+            padding = (0, self.task_state_space - tuple.shape[0])
+            tuple = np.pad(tuple, pad_width=padding, mode="constant")
+            result[index] = tuple
+        return TensorDataset(torch.Tensor(result).to(self.device))
 
-    def _get_state_space(self, task: "Task") -> int:
-        # Tuple size (current state, action, reward, successor state).
-        # TODO: Properly handle differently shaped tasks.
-        return task.state_shape[0] + 1 + 1 + task.state_shape[0]
+    def _get_state_space(self, _: "Task") -> int:
+        # TODO: Properly handle differently shaped tasks. Also, handle
+        #       variable-length state shape. Since I have hand-picked
+        #       a list of tasks, I know upfront what is the maximum
+        #       number of state features.
+        # state_space = task.state_shape[0] + 1 + 1 + task.state_shape[0]
+        return self.state_tuple_dim + 1 + 1 + self.state_tuple_dim
