d4ff4bbc750535f02941fe7788ff89faca66f2b9
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2022-06-12 15:53:23 +0200

    Updates state-space augmentation wrapper

--- src/models/wrappers.py
+++ src/models/wrappers.py
@@ -13,7 +13,8 @@ class AugmentObservationSpaceWrapper(ObservationWrapper):
         high = self.observation_space.high
         high = np.pad(high, pad_width=(0, MAX_STATE_SPACE_DIM - len(high)), mode="constant")
         low = -high
-        self._observation_space = Box(low=low, high=high, dtype=np.float32)
+        self.observation_space = Box(low=low, high=high, dtype=np.float32)
 
     def observation(self, observation):
-        return observation
+        padding = (0, MAX_STATE_SPACE_DIM - len(observation))
+        return np.pad(observation, pad_width=padding, mode="constant")
