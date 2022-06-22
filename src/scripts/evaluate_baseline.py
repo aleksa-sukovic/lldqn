97ed9e7b4e79268bbee1c8f8a8ba9f598467b667
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2022-06-22 14:37:40 +0200

    Finalizes baseline evaluation script

--- src/scripts/evaluate_baseline.py
+++ src/scripts/evaluate_baseline.py
@@ -1,9 +1,6 @@
 import os
 import sys
 import wandb
-import numpy as np
-
-from gym.wrappers.pixel_observation import PixelObservationWrapper
 
 
 # 1. Ensures modules are loaded. This assumes script is run
@@ -24,44 +21,45 @@ WANDB_TENSORBOARD = "./src/data/tensorboard"
 WANDB_GROUP="baseline"
 WANDB_JOB_TYPE="evaluation"
 TASKS = [
-    Task(
+    dict(
         env_name="Acrobot-v1",
         env_model=ControlNetwork,
         save_data_dir="./src/data/models",
         use_baseline=True,
         wrappers=[(EncodeObservation, dict())],
-        version=3,
     ),
-    Task(
+    dict(
         env_name="CartPole-v1",
         env_model=ControlNetwork,
         save_data_dir="./src/data/models",
         use_baseline=True,
         wrappers=[(EncodeObservation, dict())],
-        version=3,
     ),
 ]
 
 # 4. Train each task in a sequence.
-for task in TASKS:
-    wandb.init(
-        project=WANDB_PROJECT,
-        dir=WANDB_LOG_DIR,
-        group=task.name,
-        job_type="Policy-Evaluate",
-        name=task.save_model_name,
-        sync_tensorboard=True,
-        monitor_gym=True,
-        reinit=True,
-        config={
-            "test/episodes": 20,
-            "test/noise": False,
-        }
-    )
+for task_data in TASKS:
+    for version in range(3):
+        task = Task(**task_data, version=version + 1)
 
-    task.load()
-    result = task.collector_test.collect(n_episode=5, render = 1 / 35)
+        wandb.init(
+            project=WANDB_PROJECT,
+            dir=WANDB_LOG_DIR,
+            group=task.name,
+            job_type=f"Evaluate-Baseline",
+            name=f"{task.name}-Version-{task.version}",
+            sync_tensorboard=True,
+            monitor_gym=True,
+            reinit=True,
+            config={
+                "test/episodes": 50,
+                "test/noise": False,
+            }
+        )
 
-    wandb.run.summary["test/episodes"] = result["n/ep"]
-    wandb.run.summary["test/reward"] = result["rew"]
-    wandb.run.summary["test/reward_std"] = result["rew_std"]
+        task.load()
+        result = task.collector_test.collect(n_episode=50)
+
+        wandb.run.summary["test/episodes"] = result["n/ep"]
+        wandb.run.summary["test/reward"] = result["rew"]
+        wandb.run.summary["test/reward_std"] = result["rew_std"]
